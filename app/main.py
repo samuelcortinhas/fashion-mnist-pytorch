75c8e5b9f12a621bae95acdaa4b2afd8166fe404
Samuel Cortinhas <samuelhcortinhas@gmail.com>
2023-08-24 18:03:23 +0100

    update /predict endpoint

--- app/main.py
+++ app/main.py
@@ -21,8 +21,22 @@ with open("../config.json", "r") as f:
 # Load inference model
 model = load_model(f"../models/{cfg['inference_model_name']}.pt")
 
+# Classes
+classes = [
+    "0 T-shirt/top",
+    "1 Trouser",
+    "2 Pullover",
+    "3 Dress",
+    "4 Coat",
+    "5 Sandal",
+    "6 Shirt",
+    "7 Sneaker",
+    "8 Bag",
+    "9 Ankle boot",
+]
 
 @app.post("/predict")
 async def predict(imgs: Request):
     preds = inference(model, imgs)
-    return Response(content=preds)
+    class_preds = [classes[i] for i in preds]
+    return Response(content=class_preds)
