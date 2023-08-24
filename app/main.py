ccc447cad3c3cbd359fcada780320b1003f26c21
Samuel Cortinhas <samuelhcortinhas@gmail.com>
2023-08-24 17:40:51 +0100

    add route for inference in fastapi app

--- app/main.py
+++ app/main.py
@@ -1,4 +1,10 @@
-from fastapi import FastAPI
+import json
+import sys
+
+from fastapi import FastAPI, Request, Response
+
+sys.path.insert(0, "../src")
+from src.inference import inference, load_model
 
 app = FastAPI()
 
@@ -6,3 +12,17 @@ app = FastAPI()
 @app.get("/")
 def root():
     return {"status": "healthy"}
+
+
+# Configuration
+with open("../config.json", "r") as f:
+    cfg = json.load(f)
+
+# Load inference model
+model = load_model(f"../models/{cfg['inference_model_name']}.pt")
+
+
+@app.post("/predict")
+async def predict(imgs: Request):
+    preds = inference(model, imgs)
+    return Response(content=preds)
